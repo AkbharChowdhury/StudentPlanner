22c1c4f8a8a4fe2427dc9807fc95499cde765480
Akbhar1999 <106991949+Akbhar1999@users.noreply.github.com>
2023-05-22 12:58:39 +0100

    f

--- app/src/main/java/com/studentplanner/studentplanner/CalendarFragment.java
+++ app/src/main/java/com/studentplanner/studentplanner/CalendarFragment.java
@@ -1,64 +1,225 @@
 package com.studentplanner.studentplanner;
 
+import static com.studentplanner.studentplanner.utils.CalendarUtils.daysInMonthArray;
+import static com.studentplanner.studentplanner.utils.CalendarUtils.monthYearFromDate;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CalendarFragment extends Fragment {
+import com.studentplanner.studentplanner.adapters.EventAdapter;
+import com.studentplanner.studentplanner.models.Event;
+import com.studentplanner.studentplanner.tables.CourseworkTable;
+import com.studentplanner.studentplanner.utils.CalendarUtils;
+import com.studentplanner.studentplanner.utils.Helper;
+import com.studentplanner.studentplanner.utils.Validation;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class CalendarFragment extends Fragment implements CalendarAdapter.OnItemListener {
+    private View view;
+    private Context context;
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private ListView eventListView;
+    private DatabaseHelper db;
+
 
     public CalendarFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    }
+    private void initFragment(LayoutInflater inflater, ViewGroup container) {
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        context = getContext();
+        getActivity().setTitle(context.getString(R.string.my_calendar));
+        setHasOptionsMenu(true);
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+//        context = getContext();
+//        getActivity().setTitle("My Calendar");
+//        setHasOptionsMenu(true);
+
+        initFragment(inflater, container);
+
+
+        db = DatabaseHelper.getInstance(context);
+        initWidgets();
+        view.findViewById(R.id.btNextMonthAction).setOnClickListener(v -> nextMonthAction());
+        view.findViewById(R.id.btnPreviousMonthAction).setOnClickListener(v -> previousMonthAction());
+
+        try {
+            getEventsFromDB();
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        resetToCurrentDate();
+
+
+        ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.getSelectedDate());
+        EventAdapter eventAdapter = new EventAdapter(context, dailyEvents);
+        eventListView.setAdapter(eventAdapter);
+
+        return view;
     }
+
+    private void resetToCurrentDate() {
+        CalendarUtils.setSelectedDate(CalendarUtils.getCurrentDate());
+        setMonthView();
+    }
+
+    private void getEventsFromDB() {
+        getClassDetails();
+        getCourseworkDetails();
+    }
+
+    public void previousMonthAction() {
+        CalendarUtils.setSelectedDate(CalendarUtils.getSelectedDate().minusMonths(1));
+        setMonthView();
+    }
+
+    public void nextMonthAction() {
+        CalendarUtils.setSelectedDate(CalendarUtils.getSelectedDate().plusMonths(1));
+        setMonthView();
+    }
+
+
+    private void getClassDetails() {
+        List<Event> classes = Event.getClassesDetails(db);
+        for (Event myClass : classes) {
+            LocalDate startDate = myClass.getSemesterStartDate();
+            LocalDate endDate = myClass.getSemesterEndDate();
+            long numOfDays = ChronoUnit.DAYS.between(startDate, endDate);
+            for (LocalDate date : CalendarUtils.getRecurringEvents(numOfDays, startDate)) {
+                if (date.getDayOfWeek() == DayOfWeek.of(myClass.getDow())) {
+
+                    Event classEvent = new Event(
+                            myClass.getName(),
+                            date,
+                            myClass.getStartTime(),
+                            myClass.getEndTime(),
+                            myClass.getEventType(),
+                            myClass.getSemesterStartDate(),
+                            myClass.getSemesterStartDate(),
+                            myClass.getDow()
+                    );
+                    classEvent.setId(myClass.getId());
+                    classEvent.setClasses(myClass.getClasses());
+                    Event.getEventsList().add(classEvent);
+                }
+            }
+        }
+
+
+    }
+
+    private void getCourseworkDetails() {
+        List<Event> courseworkEvent = Event.getCourseworkDetails(db);
+        Event.getEventsList().addAll(courseworkEvent);
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = view.findViewById(R.id.calendarRecyclerView);
+        monthYearText = view.findViewById(R.id.monthYearTV);
+        eventListView = view.findViewById(R.id.eventListView1);
+
+    }
+
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.getSelectedDate()));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.getSelectedDate());
+        calendarRecyclerView.setLayoutManager(new GridLayoutManager(context, 7));
+        calendarRecyclerView.setAdapter(new CalendarAdapter(daysInMonth, this));
+        CalendarUtils.setEventAdapter(eventListView, context);
+    }
+
+
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+        CalendarUtils.setSelectedDate(date);
+        setMonthView();
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.classes_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add_coursework_action) {
+            if (Validation.isPastDate(CalendarUtils.getSelectedDate().toString())) {
+                Helper.goToActivity(getActivity(), AddCourseworkActivity.class);
+                return true;
+            }
+            // set deadline to selected calendar date
+            startForResult.launch(courseworkIntent());
+
+        }
+        if (id == R.id.add_class_action) {
+
+            Helper.goToActivity(getActivity(), AddClassesActivity.class);
+        }
+        if (id == R.id.action_week_view) {
+            Helper.goToActivity(getActivity(), WeekViewActivity.class);
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private Intent courseworkIntent() {
+        Intent intent = new Intent(getActivity(), AddCourseworkActivity.class);
+        intent.putExtra(CourseworkTable.COLUMN_DEADLINE, Helper.formatDate(CalendarUtils.getSelectedDate().toString()));
+        return intent;
+    }
+
+    private ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+
+            }
+    );
+
+
+
+
+
 }
---
+++ app/src/main/java/com/studentplanner/studentplanner/WeekViewActivity.java
@@ -0,0 +1,142 @@
+package com.studentplanner.studentplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static com.studentplanner.studentplanner.utils.CalendarUtils.daysInWeekArray;
+import static com.studentplanner.studentplanner.utils.CalendarUtils.monthYearFromDate;
+
+import com.studentplanner.studentplanner.utils.CalendarUtils;
+import com.studentplanner.studentplanner.utils.Helper;
+
+
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private ListView eventListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_view);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        initWidgets();
+        setWeekView();
+
+    }
+
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerWeekView);
+        monthYearText = findViewById(R.id.monthYearTVWeekView);
+        eventListView = findViewById(R.id.eventListView);
+    }
+
+    private void setWeekView() {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.getSelectedDate()));
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.getSelectedDate());
+        Log.d("JJJ",String.valueOf(CalendarUtils.getSelectedDate()));
+        calendarRecyclerView.setLayoutManager(new GridLayoutManager(this, 7));
+        calendarRecyclerView.setAdapter(new CalendarAdapter(days, this));
+        CalendarUtils.setEventAdapter(eventListView, this);
+
+
+    }
+
+
+    public void previousWeekAction(View view) {
+        CalendarUtils.setSelectedDate(CalendarUtils.getSelectedDate().minusWeeks(1));
+        setWeekView();
+    }
+
+    public void nextWeekAction(View view) {
+        CalendarUtils.setSelectedDate(CalendarUtils.getSelectedDate().plusWeeks(1));
+        setWeekView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+        CalendarUtils.setSelectedDate(date);
+        setWeekView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CalendarUtils.setEventAdapter(eventListView,this);
+
+    }
+
+
+
+    public void newEventAction(View view) {
+//        startActivity(new Intent(this, EventEditActivity.class));
+    }
+
+//    public void goBack(View view) {
+//        CalendarUtils.setEventAdapter(eventListView,this);
+//        finish();
+//
+//
+//
+//    }
+
+    public void todayAction(View view) {
+        CalendarUtils.setSelectedDate(CalendarUtils.getCurrentDate());
+        setWeekView();
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.classes_menu, menu);
+        MenuItem weekView = menu.findItem(R.id.action_week_view);
+        weekView.setIcon(ContextCompat.getDrawable(this, R.drawable.ic_calendar_view));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.add_coursework_action){
+            Helper.goToActivity(this, AddCourseworkActivity.class);
+        }
+        if (id == R.id.add_class_action){
+            Helper.goToActivity(this, AddClassesActivity.class);
+        }
+
+        if (id == android.R.id.home | id == R.id.action_week_view) {
+            goBack();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+    private void goBack(){
+        CalendarUtils.setEventAdapter(eventListView,this);
+        finish();
+    }
+}
