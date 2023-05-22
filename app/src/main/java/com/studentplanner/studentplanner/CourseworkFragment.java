0739e29ce075731f7485e3b36e1cc467c582893f
Akbhar1999 <106991949+Akbhar1999@users.noreply.github.com>
2023-05-22 10:00:03 +0100

    d

--- app/src/main/java/com/studentplanner/studentplanner/CourseworkFragment.java
+++ app/src/main/java/com/studentplanner/studentplanner/CourseworkFragment.java
@@ -8,11 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CourseworkFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CourseworkFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -28,31 +24,13 @@ public class CourseworkFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CourseworkFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CourseworkFragment newInstance(String param1, String param2) {
-        CourseworkFragment fragment = new CourseworkFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
--- app/src/main/java/com/studentplanner/studentplanner/MainActivity.java
+++ app/src/main/java/com/studentplanner/studentplanner/MainActivity.java
@@ -1,14 +1,17 @@
 package com.studentplanner.studentplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
 
@@ -25,18 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new ReminderFragment()).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReminderFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_reminder);
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+//        if (item.getItemId() == R.id.nav_reminder){
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReminderFragment()).commit();
+//        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
