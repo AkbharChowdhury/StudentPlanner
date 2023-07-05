40b2c1c60dac4a1f2bb46970fa3cc9266979cb3e
AkbharChowdhury <112480476+AkbharChowdhury@users.noreply.github.com>
2023-07-05 20:31:13 +0100

    s

--- app/src/main/java/com/studentplanner/studentplanner/MainActivity.java
+++ app/src/main/java/com/studentplanner/studentplanner/MainActivity.java
@@ -48,7 +48,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        navHeaderBinding = NavHeaderBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
 
